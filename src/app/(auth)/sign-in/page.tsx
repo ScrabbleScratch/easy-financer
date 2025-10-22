b6442ab9954576805e120d7719dfa6723413f0f4
Mario Orellana <ma_oa08@hotmail.com>
2025-10-22 00:57:58 -0600

    improve auth pages styles and add sign in with google button

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,20 +1,22 @@
 "use client";
 
-import { auth } from "@/app/firebase/config";
-import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
+import { useSignInWithEmailAndPassword, useSignInWithGoogle } from "react-firebase-hooks/auth";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 
+import { auth } from "@/app/firebase/config";
 import { signInSchema, SignInSchema } from "@/schemas/sign-in-schema";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 
 export default function SignInPage() {
   const router = useRouter();
   const [signInWithEmailAndPassword] = useSignInWithEmailAndPassword(auth);
+  const [signInWithGoogle] = useSignInWithGoogle(auth);
 
   const form = useForm<SignInSchema>({
     resolver: zodResolver(signInSchema),
@@ -33,42 +35,63 @@ export default function SignInPage() {
     } catch (error) {
       console.error("Error signing in:", error);
     }
-  }
+  };
+
+  const onSignInWithGoogle = async () => {
+    try {
+      const res = await signInWithGoogle();
+      if (res?.user) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.error("Error signing in with Google:", error);
+    }
+  };
 
   return (
-    <div>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)}>
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Email</FormLabel>
-                <FormControl>
-                  <Input placeholder="example@email.com" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Password</FormLabel>
-                <FormControl>
-                  <Input type="password" placeholder="********" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit">Sign In</Button>
-        </form>
-      </Form>
-      <Link href="sign-up">Don't have an account? Sign Up</Link>
+    <div className="max-w-md mx-auto my-30 p-8 border-2 rounded-3xl shadow-lg">
+      <h1 className="mb-6 text-center text-3xl font-bold">Sign In</h1>
+      <div className="flex flex-col gap-6 items-center">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="w-full flex flex-col gap-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="example@email.com" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="********" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="mt-3 flex flex-col justify-center items-center gap-6">
+              <div className="mt-3 flex flex-col justify-center items-center gap-2">
+                <Button type="submit" className="w-50 self-center">Sign In</Button>
+                <Link href="reset-password" className="w-auto text-xs">Forgot password?</Link>
+              </div>
+              <Link href="sign-up" className="w-auto text-xs">Don't have an account? <span className="text-blue-500">Sign Up</span></Link>
+            </div>
+          </form>
+        </Form>
+        <Separator className="w-full" />
+        <Button variant="outline" className="w-50" onClick={onSignInWithGoogle}>Continue with Google</Button>
+      </div>
     </div>
   );
 }
--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,21 +1,23 @@
 "use client";
 
-import { auth } from "@/app/firebase/config";
-import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
+import { useCreateUserWithEmailAndPassword, useSignInWithGoogle } from "react-firebase-hooks/auth";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 
+import { auth } from "@/app/firebase/config";
 import { signUpSchema, SignUpSchema } from "@/schemas/sign-up-schema";
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 
 export default function SignUpPage() {
   const router = useRouter()
   const [createUserWithEmailAndPassword] = useCreateUserWithEmailAndPassword(auth);
-  
+  const [signInWithGoogle] = useSignInWithGoogle(auth);
+
   const form = useForm<SignUpSchema>({
     resolver: zodResolver(signUpSchema),
     defaultValues: {
@@ -23,7 +25,7 @@ export default function SignUpPage() {
       password: "",
       confirmPassword: "",
     },
-  })
+  });
 
   const onSubmit = async (data: SignUpSchema) => {
     try {
@@ -34,55 +36,73 @@ export default function SignUpPage() {
     } catch (error) {
       console.error("Error signing up:", error);
     }
-  }
+  };
+
+  const onSignInWithGoogle = async () => {
+    try {
+      const res = await signInWithGoogle();
+      if (res?.user) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.error("Error signing in with Google:", error);
+    }
+  };
 
   return (
-    <div>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)}>
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Email</FormLabel>
-                <FormControl>
-                  <Input placeholder="example@email.com" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Password</FormLabel>
-                <FormControl>
-                  <Input type="password" placeholder="********" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="confirmPassword"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Confirm Password</FormLabel>
-                <FormControl>
-                  <Input type="password" placeholder="********" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit">Sign Up</Button>
-        </form>
-      </Form>
-      <Link href="sign-in">Already have an account? Sign In</Link>
+    <div className="max-w-md mx-auto my-30 p-8 border-2 rounded-3xl shadow-lg">
+      <h1 className="mb-6 text-center text-3xl font-bold">Sign Up</h1>
+      <div className="flex flex-col gap-6 items-center">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="w-full flex flex-col gap-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="example@email.com" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="********" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="confirmPassword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Confirm Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="********" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="mt-3 flex flex-col justify-center items-center gap-2">
+              <Button type="submit" className="w-50 self-center">Sign Up</Button>
+              <Link href="sign-in" className="w-auto text-xs">Already have an account? <span className="text-blue-500">Sign In</span></Link>
+            </div>
+          </form>
+        </Form>
+        <Separator className="w-full" />
+        <Button variant="outline" className="w-50" onClick={onSignInWithGoogle}>Continue with Google</Button>
+      </div>
     </div>
   );
 }
