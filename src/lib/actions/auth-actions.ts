36616938e406079f27f69fd4fde3d89696426c3b
Mario Orellana <ma_oa08@hotmail.com>
2025-10-25 14:23:52 -0600

    create auth actions

---
+++ src/lib/actions/auth-actions.ts
@@ -0,0 +1,25 @@
+"use server";
+
+import { PrismaClient } from "@/generated/prisma";
+
+const prisma = new PrismaClient();
+
+export async function getUser(email: string) {
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  return user;
+}
+
+export async function createUser(email: string) {
+  const user = await prisma.user.create({
+    data: {
+      email,
+    }
+  });
+
+  return user;
+}
