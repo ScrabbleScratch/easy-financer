33f6a8fa4e174f0005107824d992cdd4cc5446aa
Mario Orellana <ma_oa08@hotmail.com>
2025-10-25 14:25:15 -0600

    add temp sign out button

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,17 @@
+"use client";
+
+import { useSignOut } from "react-firebase-hooks/auth";
+
+import { auth } from "../firebase/config";
+import { Button } from "@/components/ui/button";
+
 export default function DashboardHomePage() {
-  return <div>Welcome to your dashboard!</div>;
+  const [signOut] = useSignOut(auth);
+
+  return (
+    <div>
+      <div>Welcome to your dashboard!</div>
+      <Button onClick={signOut}>Sign Out</Button>
+    </div>
+  );
 }
