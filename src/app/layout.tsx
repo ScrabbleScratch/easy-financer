519bdaa8172b3c3d5ef0da4c9e9d2e3f0f6e321e
Mario Orellana <ma_oa08@hotmail.com>
2025-10-29 00:03:27 -0600

    substitute recoil context provider with jotai context provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-import RecoilContextProvider from "./recoil-context-provider";
+import JotaiContextProvider from "@/components/providers/jotai-context-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,9 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <RecoilContextProvider>
+        <JotaiContextProvider>
           {children}
-        </RecoilContextProvider>
+        </JotaiContextProvider>
       </body>
     </html>
   );
---
+++ src/components/providers/jotai-context-provider.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { Provider } from "jotai";
+
+export default function JotaiContextProvider({
+  children,
+}: {
+  children: React.ReactNode,
+}) {
+  return (
+    <Provider>
+      {children}
+    </Provider>
+  );
+}
