4e6a24de1429b0c57337e466fb519c004e60d181
Mario Orellana <ma_oa08@hotmail.com>
2025-10-31 00:48:05 -0600

    implement validate auth state in auth pages

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,11 +1,29 @@
+"use client";
+
+import { useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { useAuthState } from "react-firebase-hooks/auth";
+
+import { auth } from "@/app/firebase/config";
+import { useRouter } from "next/navigation";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const router = useRouter();
+  const [authState, loading, error] = useAuthState(auth);
+
+  useEffect(() => {
+    if (loading) return;
+
+    if (authState) {
+      router.replace("/dashboard");
+    }
+  }, [authState, loading, error])
+
   return (
     <>
       <nav className="h-15 p-2 flex flex-row">
